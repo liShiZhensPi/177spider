eb2667acb044ef2a21595cb0a65c7b6bb79e92bf
540726169 <540726169@qq.com>
2021-01-22 18:59:22 +0800

    web

--- 177web.py
+++ 177web.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, render_template
+from flask.templating import render_template_string
 import getComic
 
 app = Flask(__name__)
@@ -13,6 +15,29 @@ def download(number):
     page_n = int(number.split("-")[-1])
     getComic.download(name,page_n)
     return getComic.success
+
+@app.route('/list')
+def list():
+    files = os.listdir('./templates')
+    print(files)
+      
+    urls = []
+    for file in files:
+        urls.append("<p><a href=\"/comic/{0}\">{1}</a></p>".format(file,file))
+    
+    text1 = """<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+</head>
+<body>
+"""
+    text2 = """</body>
+</html>"""
+    return render_template_string(text1+" ".join(urls)+text2)
+
+ 
 
 if __name__ == '__main__':
    app.run(debug = True)
